6e2a6a2bbb64e43150670e8f9fa01bd2ab5d6546
Dev Muscle First <musclefirst.iki@gmail.com>
2022-07-27 14:26:31 +0700

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div class={tw`mx-auto w-full h-screen bg-zinc-900 bg-cover bg-center relative`}
           style={`background: url("/img/home-img.webp")`}>
       <div class={tw`flex flex-col justify-center items-center w-full h-screen bg-transparent`}>
-        <a href="/" class={tw`w-auto h-auto cursor-pointer`}>
+        <a href="https://musclefirst.co.id/" target="_blank" class={tw`w-auto h-auto cursor-pointer`}>
           <div class={tw`w-auto h-auto rounded-lg bg-white hover:bg-gray-100 p-8 border border-black shadow-xl`}>
             <img
               src="/site/logo-m1.webp"
